60d3eba7d3c4053863ad6eed837f21763225bede
ohmin839 <ohmin839.echoda.resident@gmail.com>
2025-03-20 06:46:55 +0000

    [SharPlgr.WebApi] Implemented a file converter on WebAPI

--- SharPlgr/SharPlgr.WebApi/Controllers/PolytonicTextController.cs
+++ SharPlgr/SharPlgr.WebApi/Controllers/PolytonicTextController.cs
@@ -1,3 +1,5 @@
+using System.Net.Mime;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using SharPlgr.Api;
 
@@ -14,11 +16,42 @@ public class PolytonicTextController : ControllerBase
         _logger = logger;
     }
 
-    [Route("Converted")]
+    [Route("Converted/String")]
     [HttpPost]
     public string ConvertToPolytonicText([FromBody]string text)
     {
         string converted = SharPlgrApi.ConvertToPolytonicText(text);
         return converted;
+    }
+
+    [Route("Converted/File")]
+    [HttpPost]
+    public async Task<IActionResult> ConvertToPolytonicText(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+
+        var tempFilePath = Path.GetTempFileName();
+        try
+        {
+            using (var reader = new StreamReader(file.OpenReadStream(), Encoding.UTF8))
+            using (var writer = System.IO.File.AppendText(tempFilePath))
+            {
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    var converted = SharPlgrApi.ConvertToPolytonicText(line);
+                    await writer.WriteLineAsync(converted);
+                }
+            }
+            var fileBytes = await System.IO.File.ReadAllBytesAsync(tempFilePath);
+            return File(fileBytes, MediaTypeNames.Text.Plain, $"polytonic_{file.FileName}");
+        }
+        finally
+        {
+            System.IO.File.Delete(tempFilePath);
+        }
     }
 }
