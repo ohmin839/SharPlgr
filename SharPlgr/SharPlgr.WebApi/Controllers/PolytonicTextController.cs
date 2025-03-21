20d3698fcd939c84af688e180baca88d2ff971db
ohmin839 <ohmin839.echoda.resident@gmail.com>
2025-03-21 17:07:38 +0900

    [SharPlgr.WebApi] Implemented a word extractor

--- SharPlgr/SharPlgr.WebApi/Controllers/PolytonicTextController.cs
+++ SharPlgr/SharPlgr.WebApi/Controllers/PolytonicTextController.cs
@@ -1,7 +1,9 @@
 using System.Net.Mime;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SharPlgr.Api;
+using SharPlgr.WebApi.Models;
 
 namespace SharPlgr.WebApi.Controllers;
 
@@ -10,10 +12,15 @@ namespace SharPlgr.WebApi.Controllers;
 public class PolytonicTextController : ControllerBase
 {
     private readonly ILogger<PolytonicTextController> _logger;
+    private readonly ApplicationDbContext _dbContext;
 
-    public PolytonicTextController(ILogger<PolytonicTextController> logger)
+    public PolytonicTextController(
+        ILogger<PolytonicTextController> logger,
+        ApplicationDbContext dbContext
+        )
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     [Route("Converted/String")]
@@ -53,5 +60,43 @@ public class PolytonicTextController : ControllerBase
         {
             System.IO.File.Delete(tempFilePath);
         }
+    }
+
+    [Route("NewRawWords/File")]
+    [HttpPost]
+    public async Task<IActionResult> ExtractPolytonicWords(IFormFile file)
+    {
+        int newRawWords = 0;
+        using (var reader = new StreamReader(file.OpenReadStream(), Encoding.UTF8))
+        using (var tran = await _dbContext.Database.BeginTransactionAsync())
+        {
+            string? line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                var words = SharPlgrApi.CollectPolytonicWords(line);
+                foreach(var word in words)
+                {
+                    try
+                    {
+                        bool exists = await _dbContext.RawWords.AnyAsync(e => e.Word == word);
+                        if (!exists)
+                        {
+                            await _dbContext.AddAsync(new TRawWord{Word = word});
+                            await _dbContext.SaveChangesAsync();
+                            newRawWords++;
+                        }
+                    }
+                    catch
+                    {
+                        await _dbContext.Database.RollbackTransactionAsync();
+                        throw;
+                    }
+                }
+            }
+            await _dbContext.Database.CommitTransactionAsync();
+        }
+        return Ok(new {
+            newRowWords = newRawWords
+        });
     }
 }
--- SharPlgr/SharPlgr.WebApi/Program.cs
+++ SharPlgr/SharPlgr.WebApi/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Load environment variables from the .env file
+Env.Load();
+
 // Add services to the container.
+string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "";
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
